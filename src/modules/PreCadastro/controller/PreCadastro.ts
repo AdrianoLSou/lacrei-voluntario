692b8f02a99b654b88ff5c45e7cf25c03445d660
Jonathan <jonathan.wpc@gmail.com>
2022-07-08 01:37:21 -0300

    Ajuste no response da rota /lista-pendentes

--- src/modules/PreCadastro/controller/PreCadastro.ts
+++ src/modules/PreCadastro/controller/PreCadastro.ts
@@ -1,14 +1,38 @@
 import { Request, Response } from "express";
 import PROFISSOES from "../../../constants/profissoes";
-import { cadastro } from "../../../models";
+import { cadastro, consultorio, dadosPessoais, dadosProfissionais, servicos } from "../../../models";
 import bcrypt from "bcryptjs";
 
 export const listarPendentes = async (req: Request, res: Response) => {
   const list = await cadastro.instance.findAll({
     where: { aprovado: null },
-    attributes: { exclude: [
-      'senha', 'aprovado', 'dadosPessoais_id', 'dadosProfissionais_id', 'consultorio_id', 'servicos_id'
-    ]}
+    attributes: {
+      exclude: ['senha', 'dadosPessoais_id', 'dadosProfissionais_id', 'consultorio_id', 'servicos_id']
+    },
+    include: [
+      {
+        model: dadosPessoais.instance,
+        as: "dadosPessoais",
+        attributes: ["foto", "fotoDescricao", "sobre"]
+      },
+      {
+        model: dadosProfissionais.instance,
+        as: "dadosProfissionais",
+        attributes: ["especialidade", "carta", "foto"]
+      },
+      {
+        model: consultorio.instance,
+        as: "consultorio",
+        attributes: [
+
+          "celular", "fixo", "whatsapp", "tipoConsulta", "acessibilidade", "horarioFuncionamento", "convenios"]
+      },
+      {
+        model: servicos.instance,
+        as: "servicos",
+        attributes: ["nome", "duracao", "valor", "tipoConsulta"]
+      },
+    ]
   })
 
   return res.status(200).json(list);
