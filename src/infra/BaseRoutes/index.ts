26a9c9a0ddcd576760d3771cf7a42e85a2e6d7e4
Jonathan <jonathan.wpc@gmail.com>
2022-07-08 01:38:45 -0300

    Atualizacao nas rotas

--- src/infra/BaseRoutes/index.ts
+++ src/infra/BaseRoutes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import routesListaDeEspera from "../../modules/listaDeEspera/routes";
 import routesPreCadastro from "../../modules/PreCadastro/routes";
 import routesAuth from "../../modules/Auth/routes";
+import routesCadastro from "../../modules/Cadastro/routes";
 
 const routes = Router();
 
 routes.use(routesListaDeEspera);
 routes.use(routesPreCadastro);
 routes.use(routesAuth);
+routes.use(routesCadastro);
 
 export default routes;
