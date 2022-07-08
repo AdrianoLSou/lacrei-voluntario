d56fa5c364138c8e42848ff5e44c1da924416368
Jonathan <jonathan.wpc@gmail.com>
2022-07-08 01:34:50 -0300

    Configuração dos relacionamentos no ORM

--- src/models/index.ts
+++ src/models/index.ts
@@ -16,6 +16,59 @@ const consultorio = new Consultorio(mySqlConnection);
 const cadastro = new Cadastro(mySqlConnection);
 const listaEspera = new ListaEspera(mySqlConnection);
 
+dadosPessoais.instance.hasOne(cadastro.instance, {
+  constraint: true,
+  foreignKey: "dadosPessoais_id",
+  as: "dadosPessoais"
+});
+cadastro.instance.belongsTo(dadosPessoais.instance, {
+  constraint: true,
+  foreignKey: "dadosPessoais_id",
+  as: "dadosPessoais"
+});
+
+dadosProfissionais.instance.hasOne(cadastro.instance, {
+  constraint: true,
+  foreignKey: "dadosProfissionais_id",
+  as: "dadosProfissionais"
+});
+cadastro.instance.belongsTo(dadosProfissionais.instance, {
+  constraint: true,
+  foreignKey: "dadosProfissionais_id",
+  as: "dadosProfissionais"
+});
+
+servicos.instance.hasOne(cadastro.instance, {
+  constraint: true,
+  foreignKey: "servicos_id",
+  as: "servicos",
+});
+cadastro.instance.belongsTo(servicos.instance, {
+  constraint: true,
+  foreignKey: "servicos_id",
+  as: "servicos",
+});
+
+consultorio.instance.hasOne(cadastro.instance, {
+  constraint: true,
+  foreignKey: "consultorio_id",
+  as: "consultorio",
+});
+cadastro.instance.belongsTo(consultorio.instance, {
+  constraint: true,
+  foreignKey: "consultorio_id",
+  as: "consultorio",
+});
+
+enderecos.instance.hasOne(consultorio.instance, {
+  constraint: true,
+  foreignKey: "endereco_id"
+});
+consultorio.instance.belongsTo(enderecos.instance, {
+  constraint: true,
+  foreignKey: "endereco_id"
+});
+
 export {
   dadosPessoais,
   dadosProfissionais,
